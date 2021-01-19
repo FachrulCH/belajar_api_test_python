01c6501e6c62d9b9bdcde9ddd3538ec3e626d399
fachrulch <fachrul.fch@gmail.com>
2021-01-19 19:17:22 +0700

    [pelajaran 3] add assertpy

--- pelajaran_2/contoh_jawaban_2.py
+++ pelajaran_2/contoh_jawaban_2.py
@@ -24,3 +24,18 @@ def test_get_airports_by_id():
     assert data_airport["altitude"] == 239
     assert data_airport["timezone"] == "Pacific/Port_Moresby"
 
+
+def test_get_airports_by_id_wrong_assert():
+    """
+    This one is to test how it's looks like when having error
+    since we can not modify the response to be error, so what we can do is
+    to make assertion is false
+    """
+    airport_id = "LAE"
+    # action/WHEN
+    response = requests.get(f'https://airportgap.dev-tester.com/api/airports/{airport_id}')
+    data = response.json().get('data')
+    data_airport = data["attributes"]
+    # assertion/THEN
+    assert response.status_code == 200
+    assert data_airport["name"] == "Jakarta Airport"
