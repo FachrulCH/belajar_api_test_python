ff93777fc740456c2eaf696bd368455152456473
fachrulch <fachrul.fch@gmail.com>
2021-01-24 01:27:49 +0700

    [pelajaran 5] favorit test

--- pelajaran_5/api_client/favorite.py
+++ pelajaran_5/api_client/favorite.py
@@ -7,17 +7,24 @@ class FavoriteAPI(APIClient):
     def __init__(self, email, password):
         super(FavoriteAPI, self).__init__()
         self.base_url = "https://airportgap.dev-tester.com/api/favorites"
-        self.token = self.login(email, password)
+        self.token = ""
+        self.email = email
+        self.password = password
 
-    def login(self, email, password):
+    def login(self):
         token = self.post("https://airportgap.dev-tester.com/api/tokens", data={
-            "email": email,
-            "password": password
+            "email": self.email,
+            "password": self.password
         })
         assert_that(token.status_code).is_equal_to(200)
         assert_that(token.json()).contains_key("token")
-        assert_that(token.json().get("token")).is_not_empty()
+        token_value = token.json().get("token")
+        assert_that(token_value).is_not_empty()
+        self.headers["Authorization"] = f"Token {token_value}"
         return token.json().get("token")
 
     def get_all(self):
         return self.get(self.base_url)
+
+    def update_favorite_note(self, id_airport_to_modify, new_note):
+        return self.patch(f"{self.base_url}/{id_airport_to_modify}", data=new_note)
---
+++ pelajaran_5/test/favorite_test.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+
+import pytest
+from assertpy import assert_that
+
+from pelajaran_5.api_client.favorite import FavoriteAPI
+
+
+@pytest.fixture(scope="session")
+def favorite_api():
+    api = FavoriteAPI(email="naruto@mailinator.com", password="shuriken")
+    api.login()
+    return api
+
+
+def test_get_favorite(favorite_api):
+    response = favorite_api.get_all()
+    data = response.json().get("data")
+    assert_that(response.status_code).is_equal_to(200)
+    assert_that(len(data)).is_greater_than_or_equal_to(1)
+
+
+def test_update_favorite_note(favorite_api):
+    before_add = favorite_api.get_all()
+    data_before = before_add.json().get("data")
+    assert_that(len(data_before)).is_greater_than(0)
+    id_airport_to_modify = data_before[0]["id"]
+    # Add dynamic unique string with a timestamp
+    new_note = {
+        "note": f"The note for ID:{id_airport_to_modify} is changed at time: {datetime.now()} "
+    }
+
+    response = favorite_api.update_favorite_note(id_airport_to_modify, new_note)
+    assert_that(response.status_code).is_equal_to(200)
+    assert_that(response.text).contains(new_note["note"])
+
+    after_add = favorite_api.get_all()
+    data_after = after_add.json().get("data")
+    for airport_data in data_after:
+        if airport_data["id"] == id_airport_to_modify:
+            print(f"Found the modified airport {id_airport_to_modify}")
+            print("\nHere is the check for specific airport in an array data")
+            assert_that(airport_data["attributes"]["note"]).is_equal_to(new_note["note"])
